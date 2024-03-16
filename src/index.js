0d727b1af6ea520e9ec45802c7d0ed97b18d4b8c
jaypanchal261 <96589430+jaypanchal261@users.noreply.github.com>
2024-03-16 19:13:07 +0530

    utill code and error handle

--- src/index.js
+++ src/index.js
@@ -2,10 +2,24 @@
 
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({path:"./env"})
 
 
-connectDB();
+connectDB()
+.then(()=>{
+
+    app.on("error",(error)=>{
+        console.log("Error!",error)
+        throw error 
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on http://localhost:${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+   console.log("MongoDB connection failed!",err)
+})
 
 
 // import express from "express";
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+const asyncHandler = async (requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+}
+
+
+export {asyncHandler}
+
+
+
+// const asyncHandler =  (func)=>{ async()=>{} }
+
+
+// const asyncHandler = (fn)=>async(req,res,next)=>{
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: true,
+//             message: error.message
+//         })
+//     }
+// }
